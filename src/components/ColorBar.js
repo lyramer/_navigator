6f9ed47bc4070b629a056f77ffa3fe1e1e8fa05c
Derek jacoby <derekja@gmail.com>
2020-01-17 00:03:34 -0800

    fixing noData value to read N/A

--- src/components/ColorBar.js
+++ src/components/ColorBar.js
@@ -5,6 +5,8 @@ class ColorBar extends Component {
     let infoBox = {...this.props.infoBox};
     let infoBoxClass = infoBox.lat ? "infobox" : "";
     let algae = infoBox.algae;
+    if (algae) {algae = algae.toFixed(3)}
+    if (algae&&algae==1.00) {algae="N/A"}
     console.log(algae);
     return (
       <div className="colorbar">
@@ -17,7 +19,7 @@ class ColorBar extends Component {
               <div className="algaeconc">
                 <div><span className="label">Lat:</span><span>{infoBox.lat.toFixed(3)}</span></div>
                 <div><span className="label">Lon:</span><span>{infoBox.lon.toFixed(3)}</span></div>
-                <div><div className="label">Concentration:</div><div>{algae.toFixed(3)} mg/m<sup>3</sup></div></div>
+                <div><div className="label">Concentration:</div><div>{algae} mg/m<sup>3</sup></div></div>
               </div>
             }
           </div>
