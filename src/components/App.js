96307737523450ca7caa55b579fc56e5ac3c805e
lyramer <andy.wynden@gmail.com>
2021-07-27 12:19:13 -0700

    refactoring to make toy example

--- src/components/App.js
+++ src/components/App.js
@@ -1,179 +1,67 @@
-import React, { useState } from "react";
+import React from "react";
 import Map from "./Map";
-import { Layers, TileLayer, VectorLayer, ImageLayer } from "./Layers";
-import { Style, Icon } from "ol/style";
-import Feature from "ol/Feature";
+import { Layers, RasterLayer } from "./Layers";
 import Static from 'ol/source/ImageStatic';
-import Point from "ol/geom/Point";
-import { osm, vector, wmts } from "./DataSources";
 import { fromLonLat, get } from "ol/proj";
-import GeoJSON from "ol/format/GeoJSON";
 import { Controls, FullScreenControl } from "./Controls";
-import FeatureStyles from "./Features/Styles";
-import {register} from 'ol/proj/proj4';
-import proj4 from 'proj4';
-import {get as getProjection} from 'ol/proj';
-import mapConfig from "./DataSources/geojson_data.json";
 import "./App.css";
-import { getWidth, getHeight } from "ol/extent";
+import Projection from 'ol/proj/Projection'; 
+import {getCenter} from 'ol/extent'; 
 
 
-register(proj4);
-proj4.defs("EPSG:3573","+proj=laea +lat_0=90 +lon_0=-100 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs");
+const extent = [0, 0, 1024, 968];
+const projection = new Projection({
+  code: 'xkcd-image',
+  units: 'pixels',
+  extent: extent,
+});
+const newCenter = getCenter(extent);
 
-const projection = getProjection('EPSG:3573');
-
-
-
-const geojsonObject = mapConfig.geojsonObject;
-const geojsonObject2 = mapConfig.geojsonObject2;
-const markersLonLat = [mapConfig.kansasCityLonLat, mapConfig.blueSpringsLonLat];
-
-function addMarkers(lonLatArray) {
-  var iconStyle = new Style({
-    image: new Icon({
-      anchorXUnits: "fraction",
-      anchorYUnits: "pixels",
-      src: mapConfig.markerImage32,
-    }),
-  });
-  let features = lonLatArray.map((item) => {
-    let feature = new Feature({
-      geometry: new Point(fromLonLat(item)),
-    });
-    feature.setStyle(iconStyle);
-    return feature;
-  });
-  return features;
-}
-
-const newCenter = [-101.608420, 78.447431];
-//const imgExtent = transform([[-178.85389931430257, 84.6673593606421],[-41.94695494774296, 36.265663798516556]])
-const imgExtent = [[-584160.94, -115096.01], [4896529.62, -3053391.61]]
 const img = new Static({
-  url:"/assets/im052021_fcstx.png",
+  attributions: '© <a href="https://xkcd.com/license.html">xkcd</a>',
+  url: 'https://imgs.xkcd.com/comics/online_communities.png',
   projection: projection,
-  imageExtent: imgExtent,
-  imageLoadFunction: function (image, src) {
-    image.getImage().src = src;
-    image.getImage().width = getWidth(imgExtent);;
-    image.getImage().height = getHeight(imgExtent);
-  }
-})
+  imageExtent: extent,
+});
 
-console.log("img", img)
+// const imgExtent = [-134.208984375, 52.951172590256, -74.619140625, 74.044922590256]
+// const img = new Static({
+//   url:"/assets/im052021_fcstx.png",
+//   crossOrigin: '',
+//   projection: projection,
+//   imageExtent: imgExtent,
+//   // imageSize: [4000,5000]
+//   // imageLoadFunction: function (image, src) {
+//   //   image.getImage().src = src;
+//   //   image.getImage().width = getWidth(imgExtent);;
+//   //   image.getImage().height = getHeight(imgExtent);
+//   // }
+// })
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            center: mapConfig.center,
             zoom: 4,
-            wmtsData: null,
-            showLayer1: true,
-            showLayer2: false,
-            showMarker: false,
-            features: addMarkers(markersLonLat),
-            error: null
         };
     }
 
-    componentDidMount(){
-        fetch("https://cors-anywhere.herokuapp.com/http://basemap.arctic-sdi.org/mapcache/wmts/?request=GetCapabilities&service=wmts")
-            .then(res => res.text())
-            .then(async (text) => {
-                console.log(text)
-                const wmtsData = await wmts(text);
-                this.setState({ wmtsData });
-            },
-            // Note: it's important to handle errors here
-            // instead of a catch() block so that we don't swallow
-            // exceptions from actual bugs in components.
-            (error) => {
-            this.setState({
-                error
-            });
-            }
-        )
-        
-    }
-
-
-//   const [center, setCenter] = useState(mapConfig.center);
-//   const [zoom, setZoom] = useState(2);
-
-//   const [showLayer1, setShowLayer1] = useState(true);
-//   const [showLayer2, setShowLayer2] = useState(true);
-//   const [showMarker, setShowMarker] = useState(false);
-
-//   const [features, setFeatures] = useState(addMarkers(markersLonLat));
-
-
-      toggleLayerMarker = (objectID, val) => {
-        console.log(objectID + " has changed to " + val)
-            this.setState({[objectID]: val})
-      }
-
-
-
 render() {
-    let features = this.state.features;
+
     return (
         <div>
-          {/* <Map center={fromLonLat(center)} zoom={zoom}> */}
-          <Map center={fromLonLat(newCenter)} zoom={this.state.zoom}>
+          <Map 
+            center={newCenter} 
+            zoom={this.state.zoom} 
+            projection={projection}
+          >
             <Layers>
-            <TileLayer source={osm()} zIndex={0} />
-            { this.state.wmtsData && 
-            <TileLayer source={this.state.wmtsData} zIndex={0} />}
-
-              {this.state.showLayer1 && (
-                <ImageLayer
-                  source={img}
-                  style={FeatureStyles.MultiPolygon}
-                />
-              )}
-              {this.state.showLayer2 && (
-                <VectorLayer
-                  source={vector({
-                    features: new GeoJSON().readFeatures(geojsonObject2, {
-                      featureProjection: get("EPSG:3857"),
-                    }),
-                  })}
-                  style={FeatureStyles.MultiPolygon}
-                />
-              )}
-              {this.state.showMarker && <VectorLayer source={vector({ features })} />}
+                <RasterLayer source={img} />
             </Layers>
             <Controls>
               <FullScreenControl />
             </Controls>
           </Map>
-          <div>
-            <input
-              type="checkbox"
-              checked={this.state.showLayer1}
-              onChange={(event) => this.toggleLayerMarker("showLayer1", event.target.checked)}
-            />{" "}
-            Johnson County
-          </div>
-          <div>
-            <input
-              type="checkbox"
-              checked={this.state.showLayer2}
-              onChange={(event) => this.toggleLayerMarker("showLayer2", event.target.checked)}
-            />{" "}
-            Wyandotte County
-          </div>
-          <hr />
-          <div>
-            <input
-              type="checkbox"
-              checked={this.state.showMarker}
-              onChange={(event) => this.toggleLayerMarker("showMarker", event.target.checked)}
-            />{" "}
-            Show markers
-          </div>
         </div>
       );
 }
